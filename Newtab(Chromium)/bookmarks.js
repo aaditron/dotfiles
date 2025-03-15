f236bacea53a55f6a0c3a5200949732ec8150927
aaditron <aaditmaharjan01@gmail.com>
2025-03-15 22:27:02 +0545

    Start page (Jaredk3nt)

---
+++ Newtab(Chromium)/bookmarks.js
@@ -0,0 +1,27 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "GitHub", url: "https://github.com" },
+      { name: "Gmail", url: "https://inbox.google.com" },
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Drive", url: "https://drive.google.com" },
+    ],
+  },
+  {
+    title: "Reddit",
+    links: [
+      { name: "r/unixporn", url: "https://reddit.com/r/unixporn" },
+      {
+        name: "r/pcmasterrace",
+        url: "https://reddit.com/r/pcmasterrace",
+      },
+      {
+        name: "r/battlestations",
+        url: "https://reddit.com/r/battlestations",
+      },
+      { name: "r/me_irl", url: "https://reddit.com/r/me_irl" },
+    ],
+  },
+]
---
+++ Newtab(Chromium)/script.js
@@ -0,0 +1,70 @@
+const searchUrl = "https://google.com/search?q=";
+
+// Search on enter key event
+document.getElementById("search-field").addEventListener("keypress", function (e) {
+  if (e.keyCode == 13) {
+    var val = document.getElementById("search-field").value;
+    window.location.href = searchUrl + val;
+  }
+});
+
+// Get current time and format
+function getTime() {
+  let date = new Date(),
+    min = date.getMinutes(),
+    sec = date.getSeconds(),
+    hour = date.getHours();
+
+  return (
+    "" +
+    (hour < 10 ? "0" + hour : hour) +
+    ":" +
+    (min < 10 ? "0" + min : min) +
+    ":" +
+    (sec < 10 ? "0" + sec : sec)
+  );
+}
+
+
+// Handle writing out Bookmarks
+function setupBookmarks() {
+  const bookmarkContainer = document.getElementById("bookmark-container");
+  bookmarkContainer.innerHTML = bookmarks
+    .map((b) => {
+      const html = ["<div class='bookmark-set'>"];
+      html.push(`<div class="bookmark-title">${b.title}</div>`);
+      html.push('<div class="bookmark-inner-container">');
+      html.push(
+        ...b.links.map(
+          (l) =>
+            `<a class="bookmark" href="${l.url}">${l.name}</a>`
+        )
+      );
+      html.push("</div></div>");
+      return html.join("");
+    })
+    .join("");
+}
+
+window.onload = () => {
+  setupBookmarks();
+  // Set up the clock
+  document.getElementById("clock").innerHTML = getTime();
+  // Set clock interval to tick clock
+  setInterval(() => {
+    document.getElementById("clock").innerHTML = getTime();
+  }, 100);
+};
+
+document.addEventListener("keyup", (event) => {
+  if (event.keyCode == 32) {
+    // Spacebar code to open search
+    document.getElementById("search").style.display = "flex";
+    document.getElementById("search-field").focus();
+  } else if (event.keyCode == 27) {
+    // Esc to close search
+    document.getElementById("search-field").value = "";
+    document.getElementById("search-field").blur();
+    document.getElementById("search").style.display = "none";
+  }
+});
